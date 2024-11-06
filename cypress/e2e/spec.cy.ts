9da4d0bfb5ad2aa4671fa0bae05ecf0d690a3880
Ting Li <TLi26@slb.com>
2024-11-07 00:30:28 +0800

    test: update e2e test

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,15 +1,11 @@
 describe('My First Test', () => {
-  it('Visits the initial project page', () => {
-    cy.visit('/')
+  it('Visits the chatbot page', () => {
+    cy.visit('http://localhost:4200/')
 
-    cy.get('[data-cy="add-counter-btn"]').click();
-    cy.get('[data-cy="decrease-shower"]').contains('number: 0');
-    cy.get('[data-cy="increase-btn"]').click();
-    cy.get('[data-cy="increase-btn"]').click();
-    // cy.get('.btn').contains('+').click();
-    cy.get('[data-cy="decrease-shower"]').contains('number: 2');
-    cy.get('[data-cy="decrease-btn"]').click();
-
-    cy.get('[data-cy="decrease-shower"]').contains('number: 1');
+    cy.get('[data-cy="input-text-area"]').should('have.value', '我喜欢摇滚乐，给我推荐一个乐器吧');
+    cy.get('[data-cy="input-text-area"]').type('{enter}');
+    cy.get('[data-cy="input-text-area"]').should('have.value', '\n');
+    cy.get('[data-cy="question-area"]').should('contain.text', '你：我喜欢摇滚乐，给我推荐一个乐器吧');
+    cy.get('[data-cy="response-area"]').should('contain.text', '小乐：');
   })
 })
--- src/app/chatbotpage/chatbotpage.component.ts
+++ src/app/chatbotpage/chatbotpage.component.ts
@@ -1,38 +1,49 @@
 import { Component } from '@angular/core';
 import { AIGenerationService } from '../aigeneration.service';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule, MatIconButton } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { FormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+
+import { MatIcon } from '@angular/material/icon';
+import { MatGridList, MatGridTile } from '@angular/material/grid-list';
+import { MatCard, MatCardContent, MatCardHeader } from '@angular/material/card';
+import { MatMiniFabButton } from '@angular/material/button';
+import { MatOption, MatSelect } from '@angular/material/select';
+import { NgForOf } from '@angular/common';
 
 @Component({
   selector: 'app-chatbotpage',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, MatFormFieldModule, MatInputModule, MatIcon, MatGridList, MatGridTile, MatCardContent, MatCard, MatMiniFabButton, MatSelect, NgForOf, MatOption, MatCardHeader],
   templateUrl: './chatbotpage.component.html',
-  styleUrl: './chatbotpage.component.css'
+  styleUrl: './chatbotpage.component.css',
 })
 export class ChatbotpageComponent {
   inputText = '我喜欢摇滚乐，给我推荐一个乐器吧';
   question = 'Loading...';
   response = 'Loading...';
-  constructor(
-    private aiGenerationService: AIGenerationService,
-  ) {}
+  constructor(private aiGenerationService: AIGenerationService) {}
 
   onEnter(event: any): void {
-    this.question = '你：'+ event.target.value;
-    // this.response = 'Bot:'+ 
+    this.question = '你：' + event.target.value;
+    // this.response = 'Bot:'+
     this.generateResponse(this.inputText);
     event.target.value = '';
   }
 
-  private generateResponse(inputText: string){
+  private generateResponse(inputText: string) {
     this.aiGenerationService.generateContent(inputText).subscribe({
       next: (data: string) => {
-        this.response = '小乐：' + data
+        this.response = '小乐：' + data;
       },
       error: (error) => {
         console.error('Error:', error);
-      }
+      },
     });
-    
   }
-
 }
