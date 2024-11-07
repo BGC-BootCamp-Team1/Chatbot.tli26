637fef8b4245cea735b227031dcad8927d60bbbc
Ting Li <TLi26@slb.com>
2024-11-07 09:54:47 +0800

    fix: fix e2e

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,13 +1,11 @@
 describe('My First Test', () => {
   it('Visits the chatbot page', () => {
     cy.visit('http://localhost:4200/')
-    cy.intercept('POST', '/api/v1/services/aigc/text-generation/generation').as('getData');
 
     cy.get('[data-cy="input-text-area"]').should('have.value', '我喜欢摇滚乐，给我推荐一个乐器吧');
     cy.get('[data-cy="input-text-area"]').type('{enter}');
     cy.get('[data-cy="input-text-area"]').should('have.value', '\n');
     cy.get('[data-cy="question-area"]').should('contain.text', '你：我喜欢摇滚乐，给我推荐一个乐器吧');
-    cy.wait('@getData').its('response.statusCode').should('eq', 200);
-    cy.get('[data-cy="response-area"]').should('contain.text', '专业音乐人：');
+    cy.get('[data-cy="response-area"]', {timeout:15000}).should('contain.text', '专业音乐人：');
   })
 })
